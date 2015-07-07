d9b8920273443c70dabb24bf99208aa61bd412d5
Mark Repka <mark@repkam09.com>
2015-07-06 21:00:52 -0400

    Update homepage

--- index.php
+++ index.php
@@ -7,22 +7,15 @@
 <div id="wrapper">
 <?php include("/home/mark/website/template_header.php") ?>
 	<div id="extra" class="container">
-		<div id="three-column">
+		<div id="two-column">
 			<div class="boxA">
-				<div class="box"> 
-					<h2>Server of the Mines</h2>
-					<a href="/minecraft"><img src=/img/block.png></a>
-					<p>Do you play Minecraft? Check out some basic information about the server here.</p>
-				</div>
-			</div>
-			<div class="boxB">
 				<div class="box">
 					<h2>Personal Github</h2>
 					<a href="https://github.com/repkam09"><img src=/img/github.png></a>
 					<p>View my small projects and scripts. This is a work in progress still.</p>
 				</div>
 			</div>
-			<div class="boxC">
+			<div class="boxB">
 				<div class="box">
 					<h2>Mumble Server</h2>
 					<a href="mumble://repkam09.com"><img src=/img/mumble.png></a>
