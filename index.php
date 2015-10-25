66012e7221766fadc586372f7a220f7cdf25699a
repkam09 <mark@repkam09.com>
2015-10-25 19:53:12 -0400

    Readd Minecraft Server section

--- index.php
+++ index.php
@@ -8,20 +8,27 @@
 <?php include("/home/mark/website/template_header.php") ?>
 	<div id="extra" class="container">
 		<div id="three-column">
-			<div class="tbox1">
+			<div class="boxA">
 				<div class="box">
 					<h2>Personal Github</h2>
 					<a href="https://github.com/repkam09"><img src=/img/github.png></a>
 					<p>View my small projects and scripts. This is a work in progress still.</p>
 				</div>
 			</div>
-			<div class="tbox2">
+			<div class="boxB">
 				<div class="box">
 					<h2>Tempreature Monitor</h2>
 					<a href="/tools/raspi-temp-monitor/"><img src=/img/pi.png></a>
 					<p>View the running temperature monitor graph to see the temperature of my house in real(ish) time!</p>
 				</div>
 			</div>
+			<div class="boxC">
+                                <div class="box">
+                                        <h2>Minecraft Server</h2>
+                                        <a href="http://mc.repkam09.com/"><img src=/img/block.png></a>
+                                        <p>I host a Minecraft server for family and friends! Click here to see more about it.</p>
+                                </div>
+                        </div>
 		</div>
 	</div>
 </div>
