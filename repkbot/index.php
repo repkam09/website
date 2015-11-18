0299493e6821956e805dad811d1ea32bca9f8f64
Mark Repka <mark@repkam09.com>
2015-11-18 08:42:14 -0500

    Add a page for repkbot information

---
+++ repkbot/index.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+<title>RepkBot - Simple IRC Bot</title>
+<link href="/css/default.css" rel="stylesheet" type="text/css" media="all" />
+</head>
+<body>
+<div id="wrapper">
+<?php include("/home/mark/website/template_header.php") ?>
+	<div id="extra" class="container">
+		<h2>RepkBot - A simple Twitch.TV Chat Bot</h2>
+		</br>
+		<p>RepkBot is a simple, open source, irc bot written in node.js intended to provide some useful features to Twitch.TV chat channels.</p>
+		
+		<p>The bot has several commands that allow it to do things like keep track of
+			death counts, print quotes, print various 'told' and 'rekt' messages, as 
+			well as broadcast news/announcements at specified time intervals.</p>
+			
+		<p>Some commands are restricted to 'trusted' users only. These users can be added
+			to the 'trusted' list via the !trust command. The owner of a Twitch channel is
+			always a 'trusted' user for that channel and can therefore add additional trusted
+			users who can control the bot in their channel.</p>
+			
+		<p>Source code for this project can be found on <a href="https://github.com/repkam09/repbot-irc">GitHub here</a>.</p>
+
+		<p>Documentation of currently existing commands can be found in the <a href="hhttps://github.com/repkam09/repbot-irc/blob/master/README.md">readme here</a>.</p>
+		
+		<p>If you want to take a look at the basic bot features, you can type any of the bot commands in my <a href="http://twitch.tv/repkam09/chat/">twitch.tv chat page</a>.</p>
+
+		</br>
+		<p>If you have any questions, feel free to <a href="/contact">get in touch!</a></p>
+		</br>
+		</br>
+		</br>
+	</div>
+</div>
+<?php include("/home/mark/website/template_footer.php") ?>
+</body>
+</html>
