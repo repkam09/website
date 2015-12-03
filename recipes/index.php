2e1a17d7bef0eb949ddae26be821a0317c694ba6
repkam09 <mark@repkam09.com>
2015-12-03 17:22:22 -0500

    Add some recipes to my list

--- recipes/index.php
+++ recipes/index.php
@@ -14,6 +14,42 @@
 					<p><a href="http://allrecipes.com/recipe/20211/creamy-hot-cocoa/">allrecipes.com</a></p>
 				</div>
 			</div>
+			<div class="boxA">
+                                <div class="box">
+                                        <p>Big Al's K.C. Bar-B-Q Sauce</p>
+                                        <p><a href="http://allrecipes.com/recipe/44491/big-als-kc-bar-b-q-sauce/">allrecipes.com</a></p>
+                                </div>
+                        </div>
+			<div class="boxA">
+                                <div class="box">
+                                        <p>Peppermint Bark</p>
+                                        <p><a href="http://allrecipes.com/recipe/185194/peppermint-bark/">allrecipes.com</a></p>
+                                </div>
+                        </div>
+			<div class="boxA">
+                                <div class="box">
+                                        <p>No-Knead Bread</p>
+                                        <p><a href="http://cooking.nytimes.com/recipes/11376-no-knead-bread">Jim Lahey, Sullivan Street Bakery</a></p>
+                                </div>
+                        </div>
+			<div class="boxA">
+                                <div class="box">
+                                        <p>Slow Cooker Pulled Pork</p>
+                                        <p><a href="http://allrecipes.com/recipe/141678/slow-cooker-pulled-pork/">allrecipes.com</a></p>
+                                </div>
+                        </div>
+			<div class="boxA">
+                                <div class="box">
+                                        <p>Cinnamon Rolls</p>
+                                        <p><a href="http://allrecipes.com/recipe/24556/cinnamon-rolls-iii/">allrecipes.com</a></p>                              
+                                </div>
+                        </div>
+			<div class="boxA">
+                                <div class="box">
+                                        <p>Braided Nutella Star Bread</p>
+                                        <p><a href="http://www.handimania.com/cooking/braided-nutella-star-bread-all-steps.html">handimania.com</a></p>     
+                                </div>
+                        </div>
 		</div>
 	</div>
 </div>
